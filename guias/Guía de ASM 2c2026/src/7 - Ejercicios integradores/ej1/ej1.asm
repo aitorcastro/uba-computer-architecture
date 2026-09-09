6a3b469bb4e5535506592eb53fc4fc4f556a4c92
Aitor <132713433+aitorcastro@users.noreply.github.com>
2026-09-08 23:17:46 -0300

    Solucion ejercicio integrador 1a y 1b

--- guias/Guía de ASM 2c2026/src/7 - Ejercicios integradores/ej1/ej1.asm
+++ guias/Guía de ASM 2c2026/src/7 - Ejercicios integradores/ej1/ej1.asm
@@ -14,21 +14,21 @@ TRUE  EQU 1
 ; Funciones a implementar:
 ;   - es_indice_ordenado
 global EJERCICIO_1A_HECHO
-EJERCICIO_1A_HECHO: db FALSE ; Cambiar por `TRUE` para correr los tests.
+EJERCICIO_1A_HECHO: db TRUE ; Cambiar por `TRUE` para correr los tests.
 
 ; Marca el ejercicio 1B como hecho (`true`) o pendiente (`false`).
 ;
 ; Funciones a implementar:
 ;   - indice_a_inventario
 global EJERCICIO_1B_HECHO
-EJERCICIO_1B_HECHO: db FALSE ; Cambiar por `TRUE` para correr los tests.
+EJERCICIO_1B_HECHO: db TRUE ; Cambiar por `TRUE` para correr los tests.
 
 ;########### ESTOS SON LOS OFFSETS Y TAMAÑO DE LOS STRUCTS
 ; Completar las definiciones (serán revisadas por ABI enforcer):
-ITEM_NOMBRE EQU ??
-ITEM_FUERZA EQU ??
-ITEM_DURABILIDAD EQU ??
-ITEM_SIZE EQU ??
+ITEM_NOMBRE EQU 0
+ITEM_FUERZA EQU 20
+ITEM_DURABILIDAD EQU 24
+ITEM_SIZE EQU 28
 
 ;; La funcion debe verificar si una vista del inventario está correctamente 
 ;; ordenada de acuerdo a un criterio (comparador)
@@ -59,10 +59,66 @@ es_indice_ordenado:
 	; ubicación según la convención de llamada. Prestá atención a qué
 	; valores son de 64 bits y qué valores son de 32 bits o 8 bits.
 	;
-	; r/m64 = item_t**     inventario
-	; r/m64 = uint16_t*    indice
-	; r/m16 = uint16_t     tamanio
-	; r/m64 = comparador_t comparador
+	; rdi = item_t**     inventario
+	; rsi = uint16_t*    indice
+	; dx = uint16_t     tamanio
+	; rcx = comparador_t comparador
+
+	;prologo:
+	push rbp ;pila alineada
+	mov rbp, rsp ;stack frame armado
+
+	push rbx
+	push r12
+	push r13
+	push r14
+	push r15
+
+	sub rsp, 8 ; alineo la pila
+
+	;guardo parametros en no volatiles
+	mov rbx, rdi ;rbx = item_t**     inventario
+	mov r12, rsi ; r12 = uint16_t*    indice
+	movzx r13, dx ; r13 = uint16_t     tamanio
+	mov r14, rcx; r14 = comparador_t comparador
+	xor r15, r15
+	inc r15 ;r15 = j
+	sub r13, 1; r13 = tamanio -1
+
+	.ciclo:
+	mov r8, r15
+	sub r8, 1 ;r8 = i = j-1
+
+	xor rdi, rdi
+	mov di, word [r12 + r8 * 2] ;rdi = indice[i]
+	xor rsi, rsi
+	mov si, word [r12 + r15 * 2] ;rsi = indice[j]
+
+	mov rdi, [rbx + rdi * 8] ;rdi = inventaro[indice[i]]
+	mov rsi, [rbx + rsi * 8] ;rsi = inventario[indice[j]]
+
+	call r14 ;rax = cumplen_orden?
+	;si no cumplen salto
+	cmp rax, 0
+	je .return
+
+	;si cumplen, sigo (rax ya tiene el true de antes)
+	;verifico que no termine de recorrer
+	cmp r15, r13 ; j == tamanio - 1?
+	je .return ;si termine salto
+	inc r15 ;j++
+	jmp .ciclo
+	
+
+	.return:
+	;epilogo
+	add rsp, 8
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	pop rbx
+	pop rbp
 		ret
 
 ;; Dado un inventario y una vista, crear un nuevo inventario que mantenga el
@@ -91,7 +147,62 @@ indice_a_inventario:
 	; ubicación según la convención de llamada. Prestá atención a qué
 	; valores son de 64 bits y qué valores son de 32 bits o 8 bits.
 	;
-	; r/m64 = item_t**  inventario
-	; r/m64 = uint16_t* indice
-	; r/m16 = uint16_t  tamanio
+	; rdi = item_t**  inventario (puntero al inicio de un array de punteros a items)
+	; rsi = uint16_t* indice (puntero al inicio de un array de enteros de 16bits(indices))
+	; dx = uint16_t  tamanio (entero de 16 bits)
+
+	;prologo
+	push rbp ;pila alineada	
+	mov rbp, rsp
+
+	;guardo los parametros en no volatil
+	;primero salvo los registros
+	push r12
+	push r13
+	push r14
+	push r15 
+	push rbx 
+	sub rsp, 8 ;pila alineada
+
+	;ahora guardo los parametros que quiero mantener antes del malloc
+	mov r12, rdi ;r12 = inventario
+	mov r13, rsi ;r13 = indice
+	movzx rbx, dx ;rbx = tamanio
+	xor r15, r15 ;r15 = i 
+
+	;cuerpo
+	;aloco memoria
+	mov rdi, rbx ;rdi = tamanio
+	shl rdi, 3 ;rdi = tamanio * 8
+	call malloc ;rax = resultado
+	mov r14, rax ;r14= resultado
+
+	;redusco el tamanio ya que para el ciclo me sirve tamanio- 1
+	sub rbx, 1
+
+	;traslado el array
+	.ciclo:
+		;extraigo el puntero al item
+		movzx rdi, word [r13 + r15 * 2] ;rdi = indice[i]
+		mov rdi, [r12 + rdi * 8] ;rdi = inventario[indice[i]]
+
+		;pongo el puntero al item en i enresultado
+		mov [r14 + r15*8], rdi ;resultado[i] = inventario[indice[i]]
+
+		;chequeo fin de ciclo
+		;si i >= tamanio - 1 termino, si no i++
+		cmp r15, rbx 
+		je .fin
+		inc r15
+		jmp .ciclo
+
+	.fin:
+		mov rax, r14 ;rax = resultado
+	add rsp, 8
+	pop rbx
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	pop rbp
 	ret
--- guias/Guía de ASM 2c2026/src/7 - Ejercicios integradores/ej1/ej1.c
+++ guias/Guía de ASM 2c2026/src/7 - Ejercicios integradores/ej1/ej1.c
@@ -10,7 +10,7 @@
  * Funciones a implementar:
  *   - es_indice_ordenado
  */
-bool EJERCICIO_1A_HECHO = false;
+bool EJERCICIO_1A_HECHO = true;
 
 /**
  * Marca el ejercicio 1B como hecho (`true`) o pendiente (`false`).
@@ -18,13 +18,30 @@ bool EJERCICIO_1A_HECHO = false;
  * Funciones a implementar:
  *   - indice_a_inventario
  */
-bool EJERCICIO_1B_HECHO = false;
+bool EJERCICIO_1B_HECHO = true;
 
 /**
  * OPCIONAL: implementar en C
  */
 bool es_indice_ordenado(item_t** inventario, uint16_t* indice, uint16_t tamanio, comparador_t comparador) {
-	return true;
+	// tengo que recorrer el indice, buscar en el inventario los elementos a los que el indice apunta, y compararlos. a la primera puedo saltar
+	//recorro el indice
+
+	bool resultado = true;
+	for (size_t i = 0; i < tamanio - 1; i++)
+	{
+		size_t j = i + 1;
+		
+		uint16_t indice_i = indice[i];
+		uint16_t indice_j = indice[j];
+
+		item_t* puntero_i = inventario[indice_i];
+		item_t* puntero_j = inventario[indice_j];
+
+		if (!comparador(puntero_i, puntero_j)) resultado = false;
+	}
+
+	return resultado;
 }
 
 /**
@@ -32,6 +49,15 @@ bool es_indice_ordenado(item_t** inventario, uint16_t* indice, uint16_t tamanio,
  */
 item_t** indice_a_inventario(item_t** inventario, uint16_t* indice, uint16_t tamanio) {
 	// ¿Cuánta memoria hay que pedir para el resultado?
-	item_t** resultado;
+		// es un array de punteros a item luego un array de cosas de 8 bytes. Cuantas cosas? -> tamanio. Luego necesito alocar tamanio * 8
+	size_t memoriaInventarioNuevo = (size_t) tamanio * 8;
+	item_t** resultado = malloc(memoriaInventarioNuevo);
+
+	
+	for (size_t i = 0; i < tamanio; i++)
+	{
+		resultado[i] = inventario[indice[i]];
+	}
+	
 	return resultado;
 }
