e1e64b14146a029e46213db11a596262242c6d34
Aitor <132713433+aitorcastro@users.noreply.github.com>
2026-09-10 15:19:14 -0300

    Solucion ejercicio integrador 2a y 2b

--- guias/Guía de ASM 2c2026/src/7 - Ejercicios integradores/ej2/ej2.asm
+++ guias/Guía de ASM 2c2026/src/7 - Ejercicios integradores/ej2/ej2.asm
@@ -14,14 +14,14 @@ TRUE  EQU 1
 ; Funciones a implementar:
 ;   - optimizar
 global EJERCICIO_2A_HECHO
-EJERCICIO_2A_HECHO: db FALSE ; Cambiar por `TRUE` para correr los tests.
+EJERCICIO_2A_HECHO: db TRUE ; Cambiar por `TRUE` para correr los tests.
 
 ; Marca el ejercicio 1B como hecho (`true`) o pendiente (`false`).
 ;
 ; Funciones a implementar:
 ;   - contarCombustibleAsignado
 global EJERCICIO_2B_HECHO
-EJERCICIO_2B_HECHO: db FALSE ; Cambiar por `TRUE` para correr los tests.
+EJERCICIO_2B_HECHO: db TRUE ; Cambiar por `TRUE` para correr los tests.
 
 ; Marca el ejercicio 1C como hecho (`true`) o pendiente (`false`).
 ;
@@ -32,10 +32,11 @@ EJERCICIO_2C_HECHO: db FALSE ; Cambiar por `TRUE` para correr los tests.
 
 ;########### ESTOS SON LOS OFFSETS Y TAMAÑO DE LOS STRUCTS
 ; Completar las definiciones (serán revisadas por ABI enforcer):
-ATTACKUNIT_CLASE EQU ??
-ATTACKUNIT_COMBUSTIBLE EQU ??
-ATTACKUNIT_REFERENCES EQU ??
-ATTACKUNIT_SIZE EQU ??
+;offsets son
+ATTACKUNIT_CLASE EQU 0
+ATTACKUNIT_COMBUSTIBLE EQU 12
+ATTACKUNIT_REFERENCES EQU 14
+ATTACKUNIT_SIZE EQU 16
 
 global optimizar
 optimizar:
@@ -43,16 +44,201 @@ optimizar:
 	; ubicación según la convención de llamada. Prestá atención a qué
 	; valores son de 64 bits y qué valores son de 32 bits o 8 bits.
 	;
-	; r/m64 = mapa_t           mapa
-	; r/m64 = attackunit_t*    compartida
-	; r/m64 = uint32_t*        fun_hash(attackunit_t*)
+	; rdi = mapa_t           mapa (puntero a al inicio del mapa)
+	; rsi = attackunit_t*    compartida
+	; rdx = uint32_t*        fun_hash(attackunit_t*)
+
+	;--PROLOGO--
+	push rbp
+	mov rbp, rsp
+	sub rsp, 32
+	push r12
+	push r13
+	push r14
+	push r15
+
+	;guardo parametros en el stackframe ya que llamo a funhash
+	
+    mov [rbp-8], rdi   ; [rbp-8]  = mapa (8 bytes)
+    mov [rbp-16], rsi  ; [rbp-16] = compartida (8 bytes)
+    mov [rbp-24], rdx  ; [rbp-24] = fun_hash (8 bytes)
+    ; [rbp-32] no tiene nada, solo alinea
+
+	;--FIN PROLOGO--
+
+
+	;calculo el has de la compartida para comparar
+	mov rdi, rsi
+	call rdx 
+	mov r12, rax; ;r12 = hashAcomparar
+
+	;traigo los parametros
+	;mov r8, [rbp-8] ;r8 = mapa
+	;mov r9, [rbp-16] ;r9 = compartida
+	;mov r10, [rbp-24] ;r10 = funhash
+
+	;recorro el mapa
+	xor r13, r13 ;i = 0
+	.cicloi:
+		xor r14, r14 ;j = 0
+		.cicloj:
+		;BUSCO PUNTERO UNIDAD ACTUAL
+			mov r8, [rbp-8] ;r8 = mapa
+
+			mov rax, r13
+			imul rax, 255
+			add rax, r14
+			shl rax, 3
+			mov r15, [r8 + rax];r15 = pUnidadActual
+		
+		;CALCULO HASH UNIDAD ACTUAL SOLO SI NO ES NULL
+			;chequeo que no sea null
+			cmp r15, 0
+			je .sigElemento
+			;si no es null calculo hashUnidadActual
+			mov r10, [rbp-24] ;r10 = funhash
+			mov rdi, r15
+			call r10; rax = hashUnidadActual
+
+		;ya no llamo a funciones con lo cual es seguro traer los parametros
+			mov r8, [rbp-8] 	;r8 = mapa
+			mov r9, [rbp-16] 	;r9 = compartida
+			mov r10, [rbp-24] 	;r10 = funhash
+								;r12 = hashAcomparar
+								;r15 = pUnidadActual
+								;rax = hashUnidadActual
+
+		;COMPARO CON LA QUE TENGO QUE BUSCAR, SI CORRESPONDE HAGO EL CAMBIO
+			cmp  r12, rax
+			je .checkNoSonLaMisma
+			jmp .sigElemento
+			.checkNoSonLaMisma:
+			cmp r9, r15
+			je .sigElemento
+			
+			;tienen el mismo hash pero son diferentes punteros
+			;decremento las referencias de la actual
+			movzx r11, byte [r15 + 14] ;r11 es las referencias de la actual
+			cmp r11, 0
+			jle .hagoElCambio
+			dec r11
+			mov byte [r15 + 14], r11b
+
+			.hagoElCambio:
+			mov rax, r13
+			imul rax, 255
+			add rax, r14
+			shl rax, 3
+			mov [r8 + rax], r9
+
+			movzx r11, byte [r9 + 14]
+			inc r11
+			mov byte [r9 + 14], r11b
+
+			.sigElemento:
+				cmp r14, 254
+				je .sigFila
+				inc r14
+				jmp .cicloj
+		.sigFila:
+			cmp r13, 254
+			je .termineRecorrerMapa	
+			inc r13
+			jmp .cicloi
+
+	.termineRecorrerMapa:
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	add rsp, 32
+	pop rbp
 	ret
+
 
 global contarCombustibleAsignado
 contarCombustibleAsignado:
-	; r/m64 = mapa_t           mapa
-	; r/m64 = uint16_t*        fun_combustible(char*)
-	ret
+	; rdi = mapa_t           mapa
+	; rsi = uint16_t*        fun_combustible(char*)
+
+	;--PROLOGO INICIO--
+	push rbp
+	mov rbp, rsp
+	;pido espacio para guardar el res
+	sub rsp, 8
+
+	;guardo no volatiles
+	push r12
+	push r13
+	push r14
+	push r15
+	push rbx
+	
+	
+	mov dword [rbp-8], 0
+	;--PROLOGO FIN--
+
+	;guardo parametros en no volatiles
+	mov r12, rdi ;r12 = mapa
+	mov r13, rsi ;r13 = fun_combustible
+
+	;recorro el mapa
+	xor r14, r14 ;i=0
+	.cicloi:
+		xor r15, r15 ;j=0
+		.cicloj:
+			;COMPARAR COMBUSTIBLES SI CORREPONDE ACTUALIZAR RESULTADO
+			;calculo unidad actual
+			mov rbx, r14
+			imul rbx, 255
+			add rbx, r15
+			shl rbx, 3 
+			mov rbx, [rbx + r12];rbx = pUnidadActual
+
+			;chequeo que no sea null
+			cmp rbx, 0
+			je .sigElemento
+
+			;calculo combustible base
+			mov rdi, rbx
+			call r13;rax = combustibleBase
+
+			;calculo combustibleActual
+			movzx r8, word [rbx + 12] ;r8 = combustibleActual
+
+			;comparo combustibleActual > combustibleBase
+			cmp r8, rax 
+			jle .sigElemento
+			sub r8, rax
+			mov r9d, [rbp-8]
+			add r9, r8
+			mov dword [rbp-8], r9d
+
+
+			.sigElemento:
+			cmp r15, 254
+			je .sigFila
+			inc r15
+			jmp .cicloj
+
+		.sigFila:
+		cmp r14, 254
+		jge .termineRecorrerMapa
+		inc r14
+		jmp .cicloi
+
+	.termineRecorrerMapa:
+	mov eax, [rbp -8]
+	;--EPILOGO INICIO--
+	pop rbx
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	add rsp, 8
+	pop rbp
+	;--EPILOGO FIN--
+	ret 
 
 global modificarUnidad
 modificarUnidad:
--- guias/Guía de ASM 2c2026/src/7 - Ejercicios integradores/ej2/ej2.c
+++ guias/Guía de ASM 2c2026/src/7 - Ejercicios integradores/ej2/ej2.c
@@ -10,7 +10,7 @@
  * Funciones a implementar:
  *   - es_indice_ordenado
  */
-bool EJERCICIO_2A_HECHO = false;
+bool EJERCICIO_2A_HECHO = true;
 
 /**
  * Marca el ejercicio 1B como hecho (`true`) o pendiente (`false`).
@@ -18,7 +18,7 @@ bool EJERCICIO_2A_HECHO = false;
  * Funciones a implementar:
  *   - contarCombustibleAsignado
  */
-bool EJERCICIO_2B_HECHO = false;
+bool EJERCICIO_2B_HECHO = true;
 
 /**
  * Marca el ejercicio 1B como hecho (`true`) o pendiente (`false`).
@@ -32,12 +32,61 @@ bool EJERCICIO_2C_HECHO = false;
  * OPCIONAL: implementar en C
  */
 void optimizar(mapa_t mapa, attackunit_t* compartida, uint32_t (*fun_hash)(attackunit_t*)) {
+    //hash de la unidad compartida para comparar
+    uint32_t hashAComparar = fun_hash(compartida);
+
+    //recorro el mapa
+    for (size_t fila = 0; fila < 255; fila++) {
+        for (size_t columna = 0; columna < 255; columna ++) {
+            //busco puntero a unidad actual
+            attackunit_t* pUnidadActual = mapa[fila][columna];
+
+            //calculo hash unidad actual solo si es dif a null
+            if (pUnidadActual != NULL) {
+                uint32_t hashUnidadActual = fun_hash(pUnidadActual);
+
+                //comparo con la que tengo que buscar, si corresponde hago el cambio
+                if (hashAComparar == hashUnidadActual && pUnidadActual != compartida) {
+                    //decremento las referencias de la actual
+                    uint8_t ref = pUnidadActual->references;
+                    pUnidadActual->references = ref > 0? ref - 1: 0;
+                    //apunto el lugar del mapa a la compartida
+                    mapa[fila][columna] = compartida;
+                    //incremento las referencias de la compartida
+                    compartida->references++;
+                }
+            }
+
+        }
+    }
 }
 
 /**
  * OPCIONAL: implementar en C
  */
 uint32_t contarCombustibleAsignado(mapa_t mapa, uint16_t (*fun_combustible)(char*)) {
+    //tengo que calcular cuantro combustible extr atotal se uso
+    
+    uint32_t resultado = 0;
+    //recorro el mapa
+    for (size_t fila = 0; fila < 255; fila++) { 
+        for (size_t columna = 0; columna < 255; columna++) { 
+            //comparo combustible de la unidad actual, vs combustible base
+            attackunit_t* pUnidadActual = mapa[fila][columna];
+
+            if (pUnidadActual != NULL) {
+                uint16_t combustibleBase = fun_combustible(pUnidadActual->clase);
+                uint16_t combustibleActual = pUnidadActual->combustible;         
+
+                if (combustibleActual > combustibleBase) {
+                    resultado += combustibleActual - combustibleBase;
+                }
+            }
+        }
+    }
+
+    return resultado;
+
 }
 
 /**
